d7aa9e25590ae1da069309fc5540c7eb73d6e7b3
fabian sanchez <luisfabian1331@gmail.com>
2025-11-29 17:00:33 -0500

    initial windows is ready

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("OrbitSim project initialized")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Ventana:")
+
+#para que no se cierre:
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255,255,255))
+    pygame.display.flip()
+
+pygame.quit()
+
+
+
