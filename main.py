098ac398d645accf383bb873e7260f367e9f0b4f
fabian sanchez <luisfabian1331@gmail.com>
2025-11-29 21:04:26 -0500

    Implement basic linear movement for planet

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Ventana:")
 
-p1 = planetclass.planet(200, 400,(0,255,0), 40)
+p1 = planetclass.planet(200, 400,1, 1,(0,255,0), 40)
 
 #para que no se cierre:
 running = True
@@ -13,8 +13,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((255,255,255))
-    pygame.draw.circle(screen, (0, 255, 0), (p1.x, p1.y), p1.ratio)
+    screen.fill((0,0,0))
+    p1.x += p1.velocidadx
+    p1.y += p1.velocidady
+    p1.x = p1.x % 800
+    p1.y = p1.y % 600
+    pygame.draw.circle(screen, (0, 180, 0), (p1.x, p1.y), p1.ratio)
     pygame.display.flip()
 
 pygame.quit()
--- planetclass.py
+++ planetclass.py
@@ -1,6 +1,8 @@
 class planet:
-    def __init__(self, x, y, color, ratio):
+    def __init__(self, x, y, vx, vy, color, ratio):
         self.x = x
         self.y = y
+        self.velocidadx = vx
+        self.velocidady = vy
         self.color = color
         self.ratio = ratio
