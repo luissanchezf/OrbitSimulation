ecf32406543f870bd4735c171f1e5ca0123fe0c6
fabian sanchez <luisfabian1331@gmail.com>
2025-11-29 21:50:23 -0500

    Implement circle movement for planet

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 import planetclass
+from math import cos, sin
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Ventana:")
+pygame.display.set_caption("Simulador de orbitas:")
 
-p1 = planetclass.planet(200, 400,1, 1,(0,255,0), 40)
-
+p1 = planetclass.planet(200, 400,(0,255,0), 40, 300, 300, 400)
+teta = 0
 #para que no se cierre:
 running = True
 while running:
@@ -14,10 +15,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill((0,0,0))
-    p1.x += p1.velocidadx
-    p1.y += p1.velocidady
-    p1.x = p1.x % 800
-    p1.y = p1.y % 600
+    p1.x = p1.xrot + p1.ratiorot * cos(teta)
+    p1.y = p1.yrot + p1.ratiorot * sin(teta)
+    teta += 0.01
     pygame.draw.circle(screen, (0, 180, 0), (p1.x, p1.y), p1.ratio)
     pygame.display.flip()
 
--- planetclass.py
+++ planetclass.py
@@ -1,8 +1,11 @@
 class planet:
-    def __init__(self, x, y, vx, vy, color, ratio):
+    def __init__(self, x, y, color, ratio, x_rotation, y_rotation, ratio_rotation):
         self.x = x
         self.y = y
-        self.velocidadx = vx
-        self.velocidady = vy
+        self.velocidadx = 0
+        self.velocidady = 0
         self.color = color
         self.ratio = ratio
+        self.xrot = x_rotation
+        self.yrot = y_rotation
+        self.ratiorot = ratio_rotation
