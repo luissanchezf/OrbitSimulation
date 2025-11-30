dafc809f482f0b99ca64c0e035f80611e2904ac9
fabian sanchez <luisfabian1331@gmail.com>
2025-11-29 20:47:45 -0500

    put a static planet on the screen

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
-
+import planetclass
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Ventana:")
+
+p1 = planetclass.planet(200, 400,(0,255,0), 40)
 
 #para que no se cierre:
 running = True
@@ -12,6 +14,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill((255,255,255))
+    pygame.draw.circle(screen, (0, 255, 0), (p1.x, p1.y), p1.ratio)
     pygame.display.flip()
 
 pygame.quit()
