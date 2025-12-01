313727cd76b7a496f843a35d33ea39db8889ff2f
fabian sanchez <luisfabian1331@gmail.com>
2025-11-30 22:00:31 -0500

    Euler's orbit with eccentricity

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import pygame
 import planetclass
-from math import cos, sin
+from math import cos, sin, sqrt
 pygame.init()
 
 #const:
 G = 6.67430e-11
-escala = 1e11
+escala = 1e10  #1px = 1e10m
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Simulador de orbitas:")
 
-p1 = planetclass.planet(200, 300,(0,255,0), 40, 2e30, 0, 0)
-p2 = planetclass.planet(200, 200, (255,0,0),10, 6e24,3500*escala ,0)
+sol = planetclass.planet(200, 300,(0,255,0), 10, 2e30, 0, 0)
+p2 = planetclass.planet(200, 200, (255,0,0),5, 6e24,0.5 ,150*escala)
 #escala
-body = [p1,p2]
+body = [sol, p2]
+for i in range(1, len(body)):
+    if body[i].e != 1 and body[i].a != 0:
+        body[i].vy = sqrt(G * sol.masa * (1+body[i].e)/(body[i].a*(1-body[i].e)))
+    body[i].vx = 0
+    body[i].x = sol.x-body[i].a*(1-body[i].e)/escala
+    body[i].y = sol.y
+    #actualizar velocidad inicial
+
+
+
 forces = [0, 0]
 forcesx = 0
 forcesy = 0
-dt = 0.00001
+dt = 100000
 #para que no se cierre:
 running = True
 while running:
@@ -31,8 +41,8 @@ while running:
         forcesy = 0
         for j in range(len(body)):
             if i!=j:
-                dx = (body[j].x-body[i].x)
-                dy = (body[j].y-body[i].y)
+                dx = (body[j].x-body[i].x) * escala
+                dy = (body[j].y-body[i].y) * escala
                 norm = (dx)**2+(dy)**2
                 forces[i] = G*body[i].masa*body[j].masa/norm
                 forcesx += forces[i] * dx / (norm ** 0.5)
@@ -44,7 +54,7 @@ while running:
         body[i].x += body[i].vx * dt / escala
         body[i].y += body[i].vy * dt / escala
     for i in range(len(body)):
-        pygame.draw.circle(screen, body[i].color, (body[i].x, body[i].y), body[i].ratio)
+        pygame.draw.circle(screen, body[i].color, (int(body[i].x),int(body[i].y)), body[i].ratio)
     pygame.display.flip()
 
 pygame.quit()
--- planetclass.py
+++ planetclass.py
@@ -1,9 +1,14 @@
+from pygame.examples.eventlist import virtual_x
+
+
 class planet:
-    def __init__(self, x, y, color, ratio, masa, vx, vy):
+    def __init__(self, x, y, color, ratio, masa, e, a):
         self.x = x
         self.y = y
-        self.vx = vx
-        self.vy = vy
+        self.e = e
+        self.a = a
+        self.vx = 0
+        self.vy = 0
         self.color = color
         self.ratio = ratio
         self.masa = masa
